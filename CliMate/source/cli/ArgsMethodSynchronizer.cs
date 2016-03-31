0ba72e4137d14e8f472b909e2392cda213864fc8
Magnus Koch <mk@seriousgames.net>
2016-03-31 13:33:04 +0200

    arg method synchronizer tests

--- CliMate/source/cli/ArgsMethodSynchronizer.cs
+++ CliMate/source/cli/ArgsMethodSynchronizer.cs
@@ -1,14 +1,47 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using CliMate.interfaces.cli;
+using CliMate.source.cli;
 
 namespace CliMate.source.cli {
 	public class ArgsMethodSynchronizer : IArgsMethodSynchronizer {
 		
 		public bool TrySync(MethodInfo methodInfo, IList<ICliObject> args, out object[] argsSynced) {
-			argsSynced = null;
-			return true;
+			Dictionary<string,int> parameterNameOrderMap = GetParameterNameOrderMap( methodInfo.GetParameters() );
+			return Sync(args, parameterNameOrderMap, out argsSynced);
 		}	
+		
+		private bool Sync(IList<ICliObject> args, Dictionary<string,int> parameterOrderMap, out object[] argsSynced) {
+			argsSynced = null;
+
+			// Argument number mismatch
+			if(args.Count != parameterOrderMap.Keys.Count) {
+				return false;
+			}
+
+			// Argument name mismatch
+			foreach(string name in parameterOrderMap.Keys) {
+				if(args.Count( arg => arg.name == name) != 1) {
+					return false;
+				}	
+			}
+
+			argsSynced = args
+				.OrderBy( arg => parameterOrderMap[arg.name])
+				.Select( arg => arg.data)
+				.ToArray();
+					
+			return true;
+		}
+
+		private static Dictionary<string,int> GetParameterNameOrderMap(ParameterInfo[] parameters) {
+			var map = new Dictionary<string,int>();
+			for(int i=0; i<parameters.Length; i++) {
+				map[parameters[i].Name] = i;
+			}
+			return map;
+		}
 
 	}	
 }	
--- UnitTests/cli/ArgsMethodSynchronizerTests.cs
+++ UnitTests/cli/ArgsMethodSynchronizerTests.cs
@@ -1,30 +1,62 @@
-using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Reflection;
+using CliMate.interfaces.cli;
+using CliMate.source.cli;
 using NUnit.Framework;
 
 namespace Tests.cli {
 	[TestFixture]
 	public class ArgsMethodSynchronizerTests {
 
+		public void MultipleArgs(string a, string b) {
+		}
+
+		public void ArgAndOptionalArg(string a, string b = null) {
+		}
+
+		private CliObject GetArgument(string name, object data) {
+			var co = new CliObject();
+			co.name = name;
+			co.data = data;
+			return co;
+		}
+
 		[Test]
-		public void CanMatchWhenAllArgsArePresent() {
+		public void CanMatchWhenAllArgsArePresentButIncorrectlyOrdered() {
 			// Arrange
-		
+			var syncher = new ArgsMethodSynchronizer();
+			MethodInfo methodInfo = typeof(ArgsMethodSynchronizerTests).GetMethod("MultipleArgs");
+
+			object valueA = new object();
+			object valueB = new object();
+			
+			var args = new List<ICliObject> { GetArgument("b", valueB), GetArgument("a", valueA) };
+			object[] synchedArgs;
+
 			// Act
-		
+			syncher.TrySync(methodInfo, args, out synchedArgs);
+
 			// Assert
-		
+			Assert.AreSame(valueA, synchedArgs[0]);	
+			Assert.AreSame(valueB, synchedArgs[1]);	
 		}
 
 		[Test]
 		public void CanNotMatchWhenArgsAreMissing() {
 			// Arrange
-		
+			var syncher = new ArgsMethodSynchronizer();
+			MethodInfo methodInfo = typeof(ArgsMethodSynchronizerTests).GetMethod("MultipleArgs");
+
+			object valueA = new object();
+			
+			var args = new List<ICliObject> { GetArgument("a", valueA) };
+			object[] synchedArgs;
+
 			// Act
-		
+			bool canSync = syncher.TrySync(methodInfo, args, out synchedArgs);
+
 			// Assert
-		
+			Assert.That(!canSync);	
 		}
 
 		[Test]
