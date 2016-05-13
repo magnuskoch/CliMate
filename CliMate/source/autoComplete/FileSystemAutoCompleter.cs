5600e776180d2967887e0e7795bbbf346004b359
Magnus Koch <mk@seriousgames.net>
2016-05-13 10:17:00 +0200

    passing first file sytem auto completion test

--- CliMate/source/autoComplete/FileSystemAutoCompleter.cs
+++ CliMate/source/autoComplete/FileSystemAutoCompleter.cs
@@ -1,10 +1,53 @@
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace CliMate.source {
 	public class FileSystemAutoCompleter {
 
 		public IList<string> GetAutoCompletions(string input) {
-			return null;
+			input = input.Trim();
+
+			string[] split = input.Split( Path.DirectorySeparatorChar);
+			string matchedDirectory = string.Empty;
+			string trailing = string.Empty;
+			DirectoryInfo lastExistingDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()); 
+
+			// Find the last "deepest" existing directory in input
+			for(int i=0; i<split.Length; i++) {
+				string token = split[i].Trim();
+				DirectoryInfo directory = null;
+
+				if(!string.IsNullOrEmpty(token)) {
+					matchedDirectory = Path.Combine(matchedDirectory, split[i]);
+					Debug.Assert(!string.IsNullOrEmpty(matchedDirectory), string.Format("Matched directory was null or empty !"));
+					directory = new DirectoryInfo(matchedDirectory);
+				}
+
+				if(directory != null && directory.Exists) {
+					lastExistingDirectory = directory;
+				} else {
+					trailing = string.Join(Path.DirectorySeparatorChar.ToString(), split.Skip(i).ToArray());	
+					break;
+				}
+			}
+
+			// Return all the items (files and directories) that match the trailing part of input;
+			var autoCompletions = new List<string>();
+		
+			string searchPattern = string.IsNullOrEmpty(trailing) ? "" : trailing.ToLower();
+
+			// Seems lije the search pattern in GetFiles/GetDirectories is case sensitive in mono.
+			autoCompletions.AddRange( lastExistingDirectory.GetFiles().Select( file => file.Name ) );	
+			autoCompletions.AddRange( lastExistingDirectory.GetDirectories().Select( dir => dir.Name ) );	
+
+			// So let's do manual filtering
+			autoCompletions = autoCompletions.Where( item => item.ToLower().Contains(searchPattern) ).ToList();
+
+
+			return autoCompletions;
 		}	
+
 	}	
 }	
--- UnitTests/autoComplete/FileSystemAutoCompleterTests.cs
+++ UnitTests/autoComplete/FileSystemAutoCompleterTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CliMate.source;
 using NUnit.Framework;
+using System.IO;
 
 namespace Tests.autoComplete {
 
@@ -11,17 +12,34 @@ namespace Tests.autoComplete {
 		private const string TEST_DATA_PATH = "../../test-data/directory-structure";
 
 		[Test]
-		public void CanAutoCompleteOnNoInput() {
+		public void CanAutoCompleteOnRootInput() {
 			// Arrange
-			string input = string.Empty;	
-			string expected = "root";
-			int expctedNumberOfCompletions = 1;
+			string input = TEST_DATA_PATH;	
+			const string expected = "root";
+			const int expctedNumberOfCompletions = 1;
 
 			var autoCompleter = new FileSystemAutoCompleter();
 
 			// Act
 			IList<string> completions = autoCompleter.GetAutoCompletions(input);
 		
+			// Assert
+			Assert.AreEqual(expctedNumberOfCompletions, completions.Count);
+			Assert.AreEqual(expected, completions[0]);	
+		}
+
+		[Test]
+		public void CanAutoCompleteOnPartialInput() {
+			// Arrange
+			string input = Path.Combine(TEST_DATA_PATH, "RO");
+			const string expected = "root";
+			const int expctedNumberOfCompletions = 1;
+
+			var autoCompleter = new FileSystemAutoCompleter();
+
+			// Act
+			IList<string> completions = autoCompleter.GetAutoCompletions(input);
+
 			// Assert
 			Assert.AreEqual(expctedNumberOfCompletions, completions.Count);
 			Assert.AreEqual(expected, completions[0]);	
